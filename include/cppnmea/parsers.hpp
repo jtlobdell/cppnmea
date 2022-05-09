e2ad8758b6c7e52954a80705bd4b35ee0af1741f
John Lobdell <john.lobdell@gmail.com>
2022-05-08 21:43:43 -0600

    update deprecated phoenix includes

--- include/cppnmea/parsers.hpp
+++ include/cppnmea/parsers.hpp
@@ -5,9 +5,8 @@
 
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 #include <boost/fusion/include/io.hpp>
 
